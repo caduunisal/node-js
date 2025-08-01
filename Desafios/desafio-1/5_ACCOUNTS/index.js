2dd958249d7f66f925b633d68fff43c1e20089fa
caduunisal <kadu.unisal1@gmail.com>
2025-08-01 11:16:06 -0300

    Create the ACCOUNT Application

---
+++ Desafios/desafio-1/5_ACCOUNTS/index.js
@@ -0,0 +1,8 @@
+// Modulos externos
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+
+// Modulos internos
+const fs = require('fs')
+
+console.log('Aplicacao ACCOUNT rodando!')
