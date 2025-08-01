325f065b8a18ec072c9eb3527b9be9c6596d1bc3
caduunisal <kadu.unisal1@gmail.com>
2025-08-01 14:56:18 -0300

    Add create account module

--- Desafios/desafio-1/5_ACCOUNTS/index.js
+++ Desafios/desafio-1/5_ACCOUNTS/index.js
@@ -24,7 +24,53 @@ function operation() {
     ])
     .then((answer) => {
         const action = answer['action']
-        console.log(`Voce selecionou a opcao ${action}`)
+        // console.log(`Voce selecionou a opcao ${action}`)
+        if (action === 'Criar conta') {
+            creatAccount()
+        }
     })
     .catch((err => console.log(err)))
+}
+
+function creatAccount() {
+    console.log(chalk.bgGreen.black('Parabens por escolher o nosso banco!'))
+    console.log(chalk.green("Defina as opcoes da sua conta a seguir"))
+
+    buildAccount()
+}
+
+function buildAccount() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta:' 
+        }
+    ]).then(answer => {
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta ja existe. Escolha outro nome!'))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, 
+            '{"balance": 0}', 
+            function (err) {
+                console.log(err)
+            },
+                    
+        )
+
+        console.log(chalk.green('Parabens! A sua conta foi criada!'))
+        operation()
+
+    })
+    .catch(err => console.log(err))
 }
